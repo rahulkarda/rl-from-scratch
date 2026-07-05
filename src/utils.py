35351fe82bc8598c0b943e60af8cada0636e2e6e
rahulkarda <rahulkarda2002@gmail.com>
2026-07-05 07:45:18 +0000

    Fix flatten_dict: handle non-dict mapping types correctly
    Previously, flatten_dict assumed all Mapping values should be recursively flattened. Now, only dicts are flattened; other mapping types are treated as values. This avoids unintended expansion of e.g. defaultdict or custom mapping objects.

--- src/utils.py
+++ src/utils.py
@@ -137,7 +137,8 @@ def flatten_dict(d, parent_key='', sep='.'):  # flatten_dict({'a': {'b': 1}}) ->
     items = []
     for k, v in d.items():
         new_key = f"{parent_key}{sep}{k}" if parent_key else k
-        if isinstance(v, collections.abc.Mapping):
+        # Only recursively flatten if v is a dict (not generic Mapping)
+        if isinstance(v, dict):
             items.extend(flatten_dict(v, new_key, sep=sep).items())
         else:
             items.append((new_key, v))
